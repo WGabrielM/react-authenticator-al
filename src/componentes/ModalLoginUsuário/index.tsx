215b108a51a6961c6d70a06c28843eb9b0798e91
Wellington <wellingtongmb@gmail.com>
2024-07-30 15:21:03 -0300

    add axios for ModalLoginUsuario

--- src/componentes/ModalLoginUsuário/index.tsx
+++ src/componentes/ModalLoginUsuário/index.tsx
@@ -1,58 +1,79 @@
-import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks"
-import { useState } from "react"
+import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks";
+import { useState } from "react";
 
-import imagemPrincipal from './assets/login.png'
+import imagemPrincipal from "./assets/login.png";
 
-import './ModalLoginUsuario.css'
+import "./ModalLoginUsuario.css";
+import axios from "axios";
 
 interface PropsModalLoginUsuario {
-    aberta: boolean
-    aoFechar: () => void
-    aoEfetuarLogin: () => void
+  aberta: boolean;
+  aoFechar: () => void;
+  aoEfetuarLogin: () => void;
 }
 
-const ModalLoginUsuario = ({ aberta, aoFechar, aoEfetuarLogin } : PropsModalLoginUsuario) => {
+const ModalLoginUsuario = ({
+  aberta,
+  aoFechar,
+  aoEfetuarLogin,
+}: PropsModalLoginUsuario) => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
-
-    const aoSubmeterFormular = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-        const usuario = {
-            email,
-            senha,
+  const aoSubmeterFormular = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    const usuario = {
+      email,
+      senha,
+    };
+    axios
+      .post("http://localhost:8000/public/login", usuario)
+      .then((resposta) => {
+        sessionStorage.setItem("token", resposta.data.access_token);
+        setEmail("");
+        setSenha("");
+        aoEfetuarLogin();
+      })
+      .catch((erro) => {
+        if (erro?.response?.data?.message) {
+          alert(erro.response.data.message);
+        } else {
+          alert(
+            "Aconteceu um erro inesperado ao efetuar o seu login! Entre em contato com o suporte!"
+          );
         }
-        console.log(usuario)
-    }
+      });
+  };
 
-    return (<AbModal 
-        titulo="Login" 
-        aberta={aberta}
-        aoFechar={aoFechar}    
-    >
-        <section className="corpoModalCadastro">
-            <figure>
-                <img src={imagemPrincipal} alt="Pessoa segurando uma chave na frente de uma tela de computador que está exibindo uma fechadura" />
-            </figure>
-            <form onSubmit={aoSubmeterFormular}>
-                <AbCampoTexto 
-                    label="E-mail"
-                    value={email}
-                    onChange={setEmail}
-                    type="email"
-                />
-                <AbCampoTexto 
-                    label="Senha"
-                    value={senha}
-                    onChange={setSenha}
-                    type="password"
-                />
-                <div className="acoes">
-                    <AbBotao texto="Fazer login"/>
-                </div>
-            </form>
-        </section>
-    </AbModal>)
-}
+  return (
+    <AbModal titulo="Login" aberta={aberta} aoFechar={aoFechar}>
+      <section className="corpoModalCadastro">
+        <figure>
+          <img
+            src={imagemPrincipal}
+            alt="Pessoa segurando uma chave na frente de uma tela de computador que está exibindo uma fechadura"
+          />
+        </figure>
+        <form onSubmit={aoSubmeterFormular}>
+          <AbCampoTexto
+            label="E-mail"
+            value={email}
+            onChange={setEmail}
+            type="email"
+          />
+          <AbCampoTexto
+            label="Senha"
+            value={senha}
+            onChange={setSenha}
+            type="password"
+          />
+          <div className="acoes">
+            <AbBotao texto="Fazer login" />
+          </div>
+        </form>
+      </section>
+    </AbModal>
+  );
+};
 
-export default ModalLoginUsuario
+export default ModalLoginUsuario;

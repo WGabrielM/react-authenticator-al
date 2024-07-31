f3c18c372508f096284e5743e9c6662cfb880db8
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-07-31 10:34:14 -0300

    using axios to get data from API Pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,27 +1,79 @@
+import { useEffect } from "react";
 import { AbBotao } from "ds-alurabooks";
 
-import "./Pedidos.css";
+import axios from "axios";
 
+import "./Pedidos.css";
+import { IPedido } from "../../interfaces/IPedido";
 
 const Pedidos = () => {
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    axios
+      .get<IPedido[]>("http://localhost:8000/pedidos", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((resposta) => console.log(resposta.data))
+      .catch((erro) => console.log(erro));
+  }, []);
+
   return (
     <section className="pedidos">
       <h1>Meus pedidos</h1>
-      <ul className="pedido">
-        <li>
-          Pedido: <strong>123456</strong>
-        </li>
-        <li>
-          Data do pedido: <strong>12/09/2022</strong>
-        </li>
-        <li>
-          Valor total: <strong>R$ 42.00</strong>
-        </li>
-        <li>
-          Entrega realizada em: <strong>13/09/2022</strong>
-        </li>
-      </ul>
-      <AbBotao texto="Detalhes"/>
+      <div className="pedido">
+        <ul>
+          <li>
+            Pedido: <strong>123456</strong>
+          </li>
+          <li>
+            Data do pedido: <strong>12/09/2022</strong>
+          </li>
+          <li>
+            Valor total: <strong>R$ 42.00</strong>
+          </li>
+          <li>
+            Entrega realizada em: <strong>13/09/2022</strong>
+          </li>
+        </ul>
+        <AbBotao texto="Detalhes" />
+      </div>
+      <div className="pedido">
+        <ul>
+          <li>
+            Pedido: <strong>123456</strong>
+          </li>
+          <li>
+            Data do pedido: <strong>12/09/2022</strong>
+          </li>
+          <li>
+            Valor total: <strong>R$ 42.00</strong>
+          </li>
+          <li>
+            Entrega realizada em: <strong>13/09/2022</strong>
+          </li>
+        </ul>
+        <AbBotao texto="Detalhes" />
+      </div>
+      <div className="pedido">
+        <ul>
+          <li>
+            Pedido: <strong>123456</strong>
+          </li>
+          <li>
+            Data do pedido: <strong>12/09/2022</strong>
+          </li>
+          <li>
+            Valor total: <strong>R$ 42.00</strong>
+          </li>
+          <li>
+            Entrega realizada em: <strong>13/09/2022</strong>
+          </li>
+        </ul>
+        <AbBotao texto="Detalhes" />
+      </div>
     </section>
   );
 };
